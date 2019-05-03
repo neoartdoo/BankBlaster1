704e28d25950d96008b0ab786b51c15111be5c24
Marjan Panić <panic.marjan@gmail.com>
2019-05-04 00:49:17 +0200

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <!DOCTYPE html>
 <html>
     <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="main.css">
         <script src="loading.js"></script>
         <script src="load.js"></script>
